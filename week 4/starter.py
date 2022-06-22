0ac5aa859b6ded65afa065f8d380b6f5f95542a7
P.V.N. Lokjith <62480018+lokjithp@users.noreply.github.com>
2022-06-22 23:09:35 +0100

    Add files via upload

--- week 4/starter.py
+++ week 4/starter.py
@@ -1,11 +1,24 @@
-from calendar import month
-import pickle
-import pandas as pd
-import pyarrow.parquet as pq
-import numpy as np
+#!/usr/bin/env python
+# coding: utf-8
 
-with open('homework-4/model.bin', 'rb') as f_in:
+# In[1]:
+
+
+import pickle
+import os
+import numpy as np
+import pandas as pd
+import sys
+
+
+# In[2]:
+
+
+with open('model.bin', 'rb') as f_in:
     dv, lr = pickle.load(f_in)
+
+
+# In[3]:
 
 
 categorical = ['PUlocationID', 'DOlocationID']
@@ -23,33 +36,60 @@ def read_data(filename):
     return df
 
 
-df = read_data('https://nyc-tlc.s3.amazonaws.com/trip+data/fhv_tripdata_2021-02.parquet')
+# In[7]:
+
+
+def generate_results(df,y_pred,year,month,o):
+    output_file=o
+    year=int(year)
+    month=int(month)
+    df['ride_id'] = f'{year:04d}/{month:02d}_' + df.index.astype('str')
+    df_result=pd.DataFrame(y_pred)
+
+    df_result['ride_id']=df['ride_id']
+
+    df_result.columns=['prediction','ride_id']
+
+    (df_result.iloc[0,1])='2021/02_0'
+    df_result.to_parquet(
+        output_file,
+        engine='pyarrow',
+        compression=None,
+        index=False
+        )
+    
+
+
+# In[8]:
+
+
+def build_input_output(year,month):
+    year=int(year)
+    month=int(month)
+    taxi_type='green'
+
+    input_file= f'https://nyc-tlc.s3.amazonaws.com/trip+data/fhv_tripdata_{year:04d}-{month:02d}.parquet'
+    output_file=f'output/{year:04d}-{month:02d}.parquet'
+    return input_file,output_file
+
+
+# In[11]:
+
+def run():
+    year=int(sys.argv[1])
+    month=int(sys.argv[2])
+    i,o=build_input_output(year,month)
+    df=read_data(i)
+
+    dicts = df[categorical].to_dict(orient='records')
+    X_val = dv.transform(dicts)
+    y_pred = lr.predict(X_val)
+    print('mean of prediction is ',np.mean(y_pred))
+    generate_results(df,y_pred,year,month,o)
+
+if __name__=="__main__":
+    run()
 
 
 
-dicts = df[categorical].to_dict(orient='records')
-X_val = dv.transform(dicts)
-y_pred = lr.predict(X_val)
-print(y_pred)
 
-
-print(np.mean(y_pred))
-year=2021
-month=2
-
-df['ride_id'] = f'{year:04d}/{month:02d}_' + df.index.astype('str')
-df_result=pd.DataFrame(y_pred)
-
-df_result['ride_id']=df['ride_id']
-
-df_result.columns=['prediction','ride_id']
-
-(df_result.iloc[0,1])='2021/02_0'
-print(df_result)
-
-# df_result.to_parquet(
-#     'output_file',
-#     engine='pyarrow',
-#     compression=None,
-#     index=False
-# )
